187fd1f06973fb95684e90632cd6f1907db236c7
hariharaiharan <harijanakiraman12@gmail.com>
2024-09-29 21:03:46 +0530

    First commit without Project details implementations

---
+++ forms/contact.php
@@ -0,0 +1,64 @@
+<?php
+  /**
+  * Requires the "PHP Email Form" library
+  * The "PHP Email Form" library is available only in the pro version of the template
+  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
+  * For more info and help: https://bootstrapmade.com/php-email-form/
+  */
+
+  // Replace contact@example.com with your real receiving email address
+  $receiving_email_address = 'harijanakiraman12@gmail.com';
+
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library!');
+  }
+
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
+
+  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  /*
+  $contact->smtp = array(
+    'host' => 'example.com',
+    'username' => 'example',
+    'password' => 'pass',
+    'port' => '587'
+  );
+  
+  */
+
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 100);
+
+  echo $contact->send();
+
+  if (!$responses) {
+		// Where to send the mail? It should be your email address
+		$to      = $receiving_email_address;
+		// Send mail from which email address?
+		$from = $_POST['email'];
+		// Mail subject
+		$subject = $_POST['subject'];
+		// Mail message
+		$message = $_POST['message']
+		// Mail headers
+		$headers = 'From: ' . $from . "\r\n" . 'Reply-To: ' . $_POST['email'] . "\r\n" . 'X-Mailer: PHP/' . phpversion();
+		// Try to send the mail
+		if (mail($to, $subject, $message, $headers)) {
+			// Success
+			$responses[] = 'Message sent!';		
+		} else {
+			// Fail
+			$responses[] = 'Message could not be sent! Please check your mail server settings!';
+		}
+	}
+
+?>
